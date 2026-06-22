03c93914f5081d6558a1ab15842279a80d490685
Jnthzn <sanchezbenja605@gmail.com>
2026-06-22 11:22:32 -0300

    feat: se configuró sequelize y modelo Movie

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 import express from "express";
+import { startDB } from "./src/config/database.js";
+import "./src/models/movie.model.js";
 
-const app = express;
+const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+startDB();
 
 app.use("/", (req, res) => {
   return res.json({ message: "bienvenido" });
 });
 
 app.listen(PORT, async () => {
-  await console.log(`Servidor listo corriendo en https://localhost:${PORT}`);
+  await console.log(`Servidor listo corriendo en http://localhost:${PORT}`);
 });
